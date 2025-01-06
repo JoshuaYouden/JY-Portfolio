abe7c57ccf348de5fdfd1bf0dc860db21d36eb65
JoshuaYouden <162321091+JoshuaYouden@users.noreply.github.com>
2025-01-06 18:44:14 -0330

    Added files to portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// Dark Mode Toggle
+const themeToggleBtn = document.getElementById("theme-toggle");
+themeToggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
+// Shuffle Skills
+const shuffleSkillsBtn = document.getElementById("shuffle-skills");
+shuffleSkillsBtn.addEventListener("click", () => {
+  const skillsList = document.getElementById("skills-list");
+  for (let i = skillsList.children.length; i >= 0; i--) {
+    skillsList.appendChild(skillsList.children[(Math.random() * i) | 0]);
+  }
+});
